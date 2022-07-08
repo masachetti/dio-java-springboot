a8793943cc65b932f1669a314b07f08e9173f213
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-08 15:31:39 -0400

    Create address model and repository

--- src/main/java/dio/springboot/model/Address.java
+++ src/main/java/dio/springboot/model/Address.java
@@ -1,4 +1,25 @@
 package dio.springboot.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.HashMap;
+
+@Entity
+@Data
 public class Address {
+
+    @Id
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+    private HashMap<String, Object> additionalProperties = new HashMap<String, Object>();
 }
--- src/main/java/dio/springboot/repository/AddressRepository.java
+++ src/main/java/dio/springboot/repository/AddressRepository.java
@@ -1,4 +1,9 @@
 package dio.springboot.repository;
 
-public interface AddressRepository {
+import dio.springboot.model.Address;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends CrudRepository<Address, String>{
 }
